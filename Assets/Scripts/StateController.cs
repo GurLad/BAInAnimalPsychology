71f9d891b89281abf20facbee50614056e2fa94f
GurLad <40135047+GurLad@users.noreply.github.com>
2021-10-03 14:04:03 +0300

    Finished checkpoints

--- Assets/Scripts/StateController.cs
+++ Assets/Scripts/StateController.cs
@@ -6,8 +6,10 @@ public class StateController : MonoBehaviour
 {
     private static string state;
     private static StateController current;
+    private static Vector3 checkpointPos = Vector3.zero;
 
     public string LevelName;
+    public GameObject Player;
     [Header("Objects")]
     public List<GameObject> ConversationObjects;
     public List<GameObject> GameObjects;
@@ -32,10 +34,22 @@ public class StateController : MonoBehaviour
         ConversationObjects.ForEach(a => a.SetActive(false));
         GameObjects.ForEach(a => a.SetActive(true));
         CrossfadeMusicPlayer.Instance.Play(LevelName + "Game");
+        Debug.Log(checkpointPos);
+        if (checkpointPos != Vector3.zero)
+        {
+            Player.transform.position = new Vector3(checkpointPos.x, Player.transform.position.y, checkpointPos.z);
+        }
     }
 
     public static void Lose()
     {
         current.GameOver.SetActive(true);
+    }
+
+    public static void RegisterCheckpoint(Vector3 pos)
+    {
+        checkpointPos = pos;
+        checkpointPos.y = 1;
+        Debug.Log(checkpointPos);
     }
 }
